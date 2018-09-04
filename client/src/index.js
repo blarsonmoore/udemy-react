da2f47b67a0116c8475f2da9daf3c4f12c01168a
blarsonmoore <blarsonmoore@gmail.com>
2018-09-03 23:46:39 -0600

    67

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render();
+import App from "./compenents/App";
+import reducers from "./reducers";
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <provider store={store}>
+    <App />
+  </provider>,
+  document.querySelector("#root")
+);
